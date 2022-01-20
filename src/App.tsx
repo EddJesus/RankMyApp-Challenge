a2ef694ea7c3ad896f1aa86508913878c2a70da6
EddJesus <edusanto22@gmail.com>
2022-01-20 20:38:07 -0300

    feat: version of application made on code interview

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from "axios";
 import './App.css';
 
+type charactersType = {
+  id: number
+  name: string
+  image: string
+}
+
+type responseType = {
+  info: {
+    count: number
+    pages: number
+    next: string,
+    prev: string
+  }
+  results: charactersType[]
+}
+const baseUrl = "https://rickandmortyapi.com/api/character"
+
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [filteredUrl, setFilteredUrl] = useState(baseUrl)
+  const [page, setPage] = useState(1)
+  const [data, setData] = useState<responseType>()
+  const [characters, setCharacters] = useState<charactersType[]>()
+
+  const changePage = (next: boolean) => {
+    const newPage = page + (next ? 1 : -1)
+    console.log(newPage)
+    setPage(newPage)
+    setFilteredUrl(`${baseUrl}/?name=${inputValue}&page=${newPage}`)
+  }
+
+  const handleSearchByName = (name: string) => {
+    setPage(1)
+    setFilteredUrl(`${baseUrl}/?name=${name}&page=1`)
+  }
+
+  const getCharacters = useCallback( async () => {
+    try {
+      const response = await axios.get(filteredUrl)
+
+      const data: responseType = response.data
+
+      setData(data)
+      setCharacters(data.results)
+    } catch (error) {
+      window.alert(error)
+    }
+  }, [filteredUrl])
+  
+  useEffect(() => {
+    getCharacters()
+  },[getCharacters]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>{data?.info.pages}</h1>
+        <input type="" placeholder='Nome' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button onClick={() => handleSearchByName(inputValue)} >
+          Search
+        </button>
+      </div>
+      <ul>
+        {
+          characters?.map((character) => (
+            <li key={character.id}>
+              <div><p>{character.name}</p></div>
+              <div><img src={character.image} alt={character.name} /></div>
+            </li>
+          ))
+        }
+      </ul>
+      <button onClick={() => changePage(false)} disabled={page <= 1} >
+          Previous
+      </button>
+      <button onClick={() => changePage(true)} disabled={data && page >= data.info.pages}>
+          Next
+      </button>
     </div>
   );
 }
